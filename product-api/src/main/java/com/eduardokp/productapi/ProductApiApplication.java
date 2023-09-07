bbd1779a014b01d13e0581862d4e76364fdbe518
EduardoPandolfo <ekpandolfo@gmail.com>
2023-09-07 18:53:18 -0300

    project product-api created

---
+++ product-api/src/main/java/com/eduardokp/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduardokp.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiApplication.class, args);
+    }
+
+}
